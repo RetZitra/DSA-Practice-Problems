17415ad1c593c6623a566b60838c6ac2a8ca79ea
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-02-10 00:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (12.52%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,21 @@
+//Tc=O(n)
+//sc=O(n)+O(n)=O(2n)
+class Solution {
+public:
+    int f(int n,vector<int>&dp){
+   if(n<=1){
+        return n;
+   }
+   if(dp[n]!=-1){
+      return dp[n];
+   }
+   else{
+      return dp[n]=f(n-1,dp)+f(n-2,dp);
+   }
+
+}
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
+};
