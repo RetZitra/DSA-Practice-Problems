0f3d42423f4833332fcf613fabf885808de47795
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-03-10 00:00:33 +0530

    Time: 4 ms (31.40%) | Memory: 9.4 MB (8.70%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -19,6 +19,32 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return f(nums,dp,n-1);
+        // return f(nums,dp,n-1);
+        //tabulation-Tc(O(n))
+        //sc=O(n)
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int pick;
+        //     if(i>1){
+        //         pick=nums[i]+dp[i-2];
+        //     }
+        //     else{
+        //         pick=nums[i]+0;
+        //     }
+        //     int notpick=0+dp[i-1];
+        //     dp[i]=max(pick,notpick);
+        // }
+        // return dp[n-1];
+        //space optimization TC=O(n)...sc=O(1)
+        int prev2=0;
+        int prev=nums[0];
+        for(int i=1;i<n;i++){
+           int pick=nums[i]+prev2;
+           int notpick=0+prev;
+           int curri=max(pick,notpick);
+           prev2=prev;
+           prev=curri;
+        }
+        return prev;
     }
 };
