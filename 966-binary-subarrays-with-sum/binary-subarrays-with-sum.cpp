0d70cf8362f20af17e50d2a146e02a1796d6da93
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-04-02 21:48:20 +0530

    Time: 21 ms (82.33%) | Memory: 31.1 MB (78.31%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-//same pattern with leetcode 560
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int>prefixsum;
-        int sum=0;
+        //tc=0(2n)+0(2n)=0(4n)
+        //sc=0(1)
         int n=nums.size();
-        int count=0;
-        prefixsum[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            int rem=(sum-goal);
-            if(prefixsum.find(rem)!=prefixsum.end()){
-                count+=prefixsum[rem];
+        int i=0;
+        int j=0;
+        int cnt1=0;
+        int cnt2=0;
+        int sum=0;
+        while(j<n){
+            sum+=nums[j];
+            //sliding window take valid korchi jate kore sum ta goal range er moddhe thake
+            while(sum>goal){
+                sum-=nums[i];
+                i++;
             }
-            prefixsum[sum]++;
+            //valid korar por amra kokhonoi exactly sum==goal pabo na..jeheto goal take exceed
+            //korte dicchi na tai subarray pete pari <=goal not exact goal ..after validation
+            //of window
+            cnt1+=(j-i+1);
+            j++;
         }
-        return count;
+        //jeheto exact sum=k pabo na tai subarrays with sum<=goal(cnt1) bar korchi..tarpr subarrays sum <=goal-1(cnt2)
+        //bar korchi die cnt1-cnt2 korlei peye jacchi subarrays with sum exactly k
+        i=0;
+        j=0;
+        sum=0;
+        while(j<n){
+            //edge case
+            if(goal-1<0){
+                cnt2=0;
+                break;
+            }
+            sum+=nums[j];
+            while(sum>(goal-1)){
+                sum-=nums[i];
+                i++;
+            }
+            cnt2+=(j-i+1);
+            j++;
+        }
+        return (cnt1-cnt2);
     }
 };
