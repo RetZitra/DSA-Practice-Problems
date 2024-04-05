a6cb6c1d5f6ae977597a6743b0eed47d43c35f63
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-04-05 20:50:15 +0530

    Time: 12 ms (85.16%) | Memory: 16.5 MB (95.25%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    //ei problem tai basically directed graph er edge gulo die dieche..
+    //to amk findout korte hobe,je jodi toposort kora jai mane graph ta cyclic na hoi thle true return korte 
+    //ar jodi toposort kora na jai mane graph ta cyclic hoi..thle false return korte
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //numcourse ta holo number of course
         int v=numCourses;
         int e=prerequisites.size();
         vector<int>adj[v];
         int c=0;
         queue<int>q;
+        //adjacency list banie nicchi
+        //ekhane boleche jeta second e deoa thekbe seta first e asbe to v->u ta deoa ache tai sei vhabe adjacency list
+        //ta banalam
         for(int i=0;i<e;i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
+            int v=prerequisites[i][0];
+            int u=prerequisites[i][1];
             adj[u].push_back(v);
         }
         int indegree[v];
