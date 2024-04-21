074e6689f291e1033074a0c2ee64b054f1b43d63
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-04-21 20:47:48 +0530

    Time: 5 ms (85.12%) | Memory: 21.8 MB (14.82%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,35 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//tc=O(n^2)
 class Solution {
 public:
-//karon recursion e jodi true and false return kori to ami oi node ta je height create korche seta return korte parchi na caller function ke..
-//tar joono oi node er moddhe height pabar jonno abr amk n times oi node er joono dfs kore height pete hocche
-//jodi amr dfs bole je left subtree and right subtree er height er difference 1 er cheye boro thle
-//-1 return korbo..tar mane holo je tree tar height balanced noi
-//ar nahole tree er height return korbo..karon jodi ota balanced hoi thle ei case e asbe ek..ar second amke tar ager node er height ta lagbe tai child node ta jodi balanced hoi to parent
-//node ta balanced kina seta dekhar jonno tar height ta take dite hobe parent node e tobei se check korte parbe..se balanced kina
-    int dfs(TreeNode* node){
-        if(node==NULL){
-            return 0;
-        }
-        int lefth=dfs(node->left);
-        int righth=dfs(node->right);
-        //left subtree ba right subtree er height jodi balanced na hoi to ar dekhte hobe na puro tree tai unbalanced 
-        if(lefth==-1 || righth==-1){
-            return -1;
-        }
-        //jodi current node ta unbalanced hoi to -1 die track korbo
-        if(abs(lefth-righth)>1){
-            return -1;
-        }
-        return (1+max(lefth,righth));
+//O(n^2) approach
+//these function gives the height of a node
+int dfs(TreeNode* node){
+    if(node==NULL){
+        return 0;
     }
-    bool isBalanced(TreeNode* root) {
-        int height=dfs(root);
-        if(height==-1){
+    int lefth=dfs(node->left);
+    int righth=dfs(node->right);
+    return max(lefth,righth)+1;
+}
+//these function check that tree is balanced or not
+//O(n)
+bool check(TreeNode* node){
+    //recursion jeheto sesh thek suru hoi tai dhore nicchi je..sob node er height balanced..base case
+    if(node==NULL){
+        return true;
+    }
+    //jodi ekta node er left subtree ba right subtree bole je..ami height balanced noi..to puro tree ta height balanced noi
+        if(check(node->left)==false||check(node->right)==false){
             return false;
         }
+        //o(n)
+        //left subtree ar right subtree er height bar korchi
+        int lefth=dfs(node->left);
+        int righth=dfs(node->right);
+        //height balanced er condition check korchi..jodi statisfy na kore to false
+        if(abs(lefth-righth)>1){
+            return false;
+        }
+        //jodi height balanced er sob codition statisfy kore to oi node tar jonno true..oi node ta height-balanced
         return true;
+}
+    bool isBalanced(TreeNode* root) {
+        if(check(root)){
+            return true;
+        }
+        return false;
     }
 };
