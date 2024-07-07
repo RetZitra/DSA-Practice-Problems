60fc52b1267fb5692f10c744230d03e62a89348a
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-07-07 21:15:25 +0530

    Time: 3 ms (16.46%) | Memory: 7.2 MB (31.20%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+//i ta 1 theke start korchi every second e next pelow te jacche ar time ta komche
+//jodi nth pellow te jai to abr process ta ulto hocche mne nth position theke 1th position e asche
+//ei forward ar backward move hotei thakche jotokhon na time ta sesh hoi
+//time ta sesh hoi gele jei position e asche pillow ta sei position ta return korchi
     int passThePillow(int n, int time) {
-       if(time<n){
-        return 1+time;
-       }
-       int initial_pos=1;
-       int rotate_time_to_initial_pos=(n-1)*2;
-       int extra_rotate;
-       if(time>rotate_time_to_initial_pos){
-        extra_rotate=time%rotate_time_to_initial_pos;
-       }
-       else{
-        extra_rotate=rotate_time_to_initial_pos%time;
-       }
-       if(extra_rotate>=n){
-        return (1+(rotate_time_to_initial_pos-extra_rotate));
-       }
-       int current_pos=initial_pos+extra_rotate;
-       return current_pos;
+        int i=1;
+        while(time>0){
+            if(i==1){
+                while(time>0 && i<n){
+                   i++;
+                   time--;
+                }
+            }
+            if(i==n){
+               while(time>0 && i>1){
+                time--;
+                i--;
+               }
+            }
+        }
+        return i;
     }
 };
