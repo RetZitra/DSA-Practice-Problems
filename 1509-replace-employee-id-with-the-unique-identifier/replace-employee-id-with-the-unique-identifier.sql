34bc44bb7cc637ac24d8d38cb5bf77ecfd12c8d9
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-09-11 00:47:58 +0530

    Time: 1151 ms (53.47%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
+-- jokhon erom hobe je 2to table er join kore je value gulo null sei gulo o dekhate hobe tar mane jeta 2to table e common nei seta charao onno value gulo o show korte hobe
+-- tokhon amra jodi left table er extra value gulo dekhate bole left join right table er extra value gulo dekhate bole to right join ar both table er extra data dekthate bolle full outer join
+-- marbo
 select EmployeeUNI.unique_id,Employees.name from Employees LEFT JOIN EmployeeUNI on Employees.id=EmployeeUNI.id
