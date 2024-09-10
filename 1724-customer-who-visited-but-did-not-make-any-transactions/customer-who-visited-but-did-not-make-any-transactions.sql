0576f50ffa40e2957c8be09ce673d5457bc21c44
Aritra Rana <127825506+RetZitra@users.noreply.github.com>
2024-09-10 17:18:20 +0530

    Time: 1359 ms (42.14%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
 -- subquery
+-- mne abr visits table theke sei sob visit id gulo chai jegilo transaction table e nei
+-- inner query die korle age transaction table theke sob visitid gulo bar kore nebo die bolbo je amr visits table theke sei sob 
+-- visit_id gulo chai jegilo transaction table e nei using not in command
+-- ebar oi data gulo peye gele oi data er ki ki dekhate boleche sei gulo dekhabo
+-- ekhane count ta customer er respect e groupby kore dekhate boleche
 -- Select customer_id,count(visit_id) as count_no_trans from Visits where visit_id not in(select visit_id from Transactions)group by customer_id;
 -- join
+-- join dieo same bolchi je ami 2to table join kore nile jei visit_id gulo 2to table e common sei gulo thakbe ebr ami visits table er sei sob visit id bar korbo
+-- jegolo ei join table e nei..sei sei data ba touple gulo peye gele ami nijer iccha moto data ja ja column dekhate bolbe segulo dekhabo
 select customer_id,count(visit_id)as count_no_trans from Visits where visit_id not in(select Transactions. visit_id from Visits INNER JOIN Transactions on 
 Visits.visit_id=Transactions.visit_id)group by customer_id
